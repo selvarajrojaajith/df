bf546072a9a995620cd422c7786f3e79bac5c3ef
selvarajrojaajith <36071813+selvarajrojaajith@users.noreply.github.com>
2018-03-19 16:08:39 +0530

    string.c
    good

---
+++ string.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char a[100],b[100];
+printf("Enter the string\n");
+gets[a];
+printf("Enter the string\n");
+gets[b];
+if(strcmp(a,b)==0)
+{
+printf("Enter the string are equal.\n");
+return 0;
+}
